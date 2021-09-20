3723e01f51f880be3396bca261f85281c64ed0e0
Felipe <felipe@superclient.com.br>
2021-09-19 21:38:21 -0300

    movimentos nas peças

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 from peao import Peao
+from enum import Enum
 
 pygame.init()
 
@@ -9,11 +10,17 @@ BRANCO = (255,255,255)
 WIDTH, HEIGTH = 900, 900
 BOARD = pygame.transform.scale(pygame.image.load(os.path.join("assets", "board.png")), (WIDTH, HEIGTH))
 
+class Turnos(Enum):
+    JOGADOR_1 = 0
+    JOGADOR_2 = 1
+    
+
 class Game:
     def __init__(self):
         self.w = WIDTH
         self.h = HEIGTH
         self.pecas = [Peao('azul', 0), Peao('vermelha', 1)]
+        self.turno = Turnos.JOGADOR_1
     
     def run(self):
         self.win = pygame.display.set_mode((self.w,self.h))
@@ -23,12 +30,32 @@ class Game:
             event = pygame.event.poll()
             if event.type == pygame.QUIT:
                 break
+            
+            if self.turno == Turnos.JOGADOR_1:
+                peca = self.pecas[0]
+                
+            if self.turno == Turnos.JOGADOR_2:
+                peca = self.pecas[1]
+                
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                peca.mover(pos)
+                
             self.draw()
+            # self.mover(0,0)
     
     def draw(self):
         self.win.blit(BOARD, (0,0))
         for peca in self.pecas:
             peca.draw(self.win)
+        
+        if self.turno == Turnos.JOGADOR_1:
+            peca = self.pecas[0]
+            
+        if self.turno == Turnos.JOGADOR_2:
+            peca = self.pecas[1]
+            
+        peca.desenhar_movimentos(self.win)
         pygame.display.update()
 
 
--- peao.py
+++ peao.py
@@ -5,18 +5,73 @@ WIN_WIDTH, WIN_HEIGTH = 900, 900
 MARGEM = int(WIN_WIDTH*.1)
 TAMANHO_CASA = 60 
 TAMANHO_BARREIRA = 20
-VERMELHA = pygame.transform.scale(pygame.image.load(os.path.join("assets", "vermelha.png")), (TAMANHO_CASA, TAMANHO_CASA))
-AZUL = pygame.transform.scale(pygame.image.load(os.path.join("assets", "azul.png")), (TAMANHO_CASA, TAMANHO_CASA))
 
-cores = {'vermelha': VERMELHA, 'azul': AZUL}
+PECA_VERMELHA = pygame.transform.scale(pygame.image.load(os.path.join("assets", "vermelha.png")), (TAMANHO_CASA, TAMANHO_CASA))
+PECA_AZUL = pygame.transform.scale(pygame.image.load(os.path.join("assets", "azul.png")), (TAMANHO_CASA, TAMANHO_CASA))
+
+VERMELHO = (255, 0, 0)
+AZUL = (0, 0, 255)
+
+pecas = {'vermelha': PECA_VERMELHA, 'azul': PECA_AZUL}
+cores = {'vermelha': VERMELHO, 'azul': AZUL}
 jogadores = [(4,0), (4,8)]
 
 class Peao:
     def __init__(self, cor: str, numero_jogador: int):
-        self.img = cores[cor]
+        self.cor = cores[cor]
+        self.img = pecas[cor]
         self.x, self.y = jogadores[numero_jogador]
+        print(transform_coord((self.x - 1, self.y)))
     
     def draw(self, win):
-        pos_x = MARGEM + TAMANHO_BARREIRA/2 + self.x * (TAMANHO_CASA + TAMANHO_BARREIRA)
-        pos_y = MARGEM + TAMANHO_BARREIRA/2 + self.y * (TAMANHO_CASA + TAMANHO_BARREIRA)
-        win.blit(self.img, (pos_x, pos_y))
+        coords = transform_coord((self.x, self.y))
+        win.blit(self.img, coords)
+        
+    def movimentos_possiveis(self):
+        movimentos = []
+        # cima
+        if self.x - 1 >= 0:
+            movimentos.append((self.x - 1, self.y))
+        # baixo
+        if self.x + 1 <= 8:
+            movimentos.append((self.x + 1, self.y))
+        # esquerda
+        if self.y - 1 >= 0:
+            movimentos.append((self.x, self.y - 1))
+        # direita
+        if self.y + 1 <= 8:
+            movimentos.append((self.x, self.y + 1))
+        
+        return movimentos
+
+    def desenhar_movimentos(self, win):
+        for movimento in self.movimentos_possiveis():
+            pos_x = MARGEM + (TAMANHO_BARREIRA + TAMANHO_CASA)/2 + movimento[0] * (TAMANHO_CASA + TAMANHO_BARREIRA)
+            pos_y = MARGEM + (TAMANHO_BARREIRA + TAMANHO_CASA)/2 + movimento[1] * (TAMANHO_CASA + TAMANHO_BARREIRA)
+            pygame.draw.circle(win, self.cor, (pos_x, pos_y), 10)
+    
+    def mover(self, pos: tuple):
+        # convertendo posição do mouse para coordenada no tabuleiro
+        casa_x = (pos[0] - MARGEM) // (TAMANHO_BARREIRA + TAMANHO_CASA)
+        casa_y = (pos[1] - MARGEM) // (TAMANHO_BARREIRA + TAMANHO_CASA)
+        
+        if not 0 <= casa_x <= 8 or not 0 <= casa_y <= 8:
+            return
+        
+        movimentos_possiveis = self.movimentos_possiveis()
+        nova_pos = (casa_x, casa_y)
+        
+        if not nova_pos in movimentos_possiveis:
+            return
+        
+        self.x, self.y = nova_pos
+        print((casa_x,casa_y))
+        return True
+
+        
+        
+        
+def transform_coord(coord: tuple) -> tuple:
+    pos_x = MARGEM + TAMANHO_BARREIRA/2 + coord[0] * (TAMANHO_CASA + TAMANHO_BARREIRA)
+    pos_y = MARGEM + TAMANHO_BARREIRA/2 + coord[1] * (TAMANHO_CASA + TAMANHO_BARREIRA)
+    return (pos_x, pos_y)
